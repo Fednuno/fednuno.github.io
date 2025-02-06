602482d315e44c1aba173c2ce88d6c1d53c5a05e
Fednuno <vasanthschemopedia@gmail.com>
2025-02-06 21:33:52 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll(".sidebar a").forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            let target = document.querySelector(this.getAttribute("href"));
+            window.scrollTo({ top: target.offsetTop - 50, behavior: "smooth" });
+        });
+    });
+});
